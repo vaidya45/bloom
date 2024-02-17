221403881b98dfdb2d7f3608313c438426d324e2
vaidya45 <103296422+vaidya45@users.noreply.github.com>
2024-02-16 22:36:44 -0500

    Added INDEX_VALUE for constant + style

--- lib/models/course.model.ts
+++ lib/models/course.model.ts
@@ -1,39 +1,40 @@
 import mongoose from 'mongoose';
 
 const courseSchema = new mongoose.Schema({
-    name: {type: String, required: true, unique: true}, 
-    link: {type: String, required: true}, 
-    title: {type: String, required: true},
+    name: { type: String, required: true, unique: true },
+    link: { type: String, required: true },
+    title: { type: String, required: true },
+    INDEX_VALUE: { type: String, required: false },
     sections: [
         {
-            sectionId: {type: String, required: true},
-            totalSeats: {type: String, required: true},
-            instructor: {type: String, required: true},
+            sectionId: { type: String, required: true },
+            totalSeats: { type: String, required: true },
+            instructor: { type: String, required: true },
             waitlistHistory: [
                 {
-                    waitlistCount: {type: Number, required: true},
-                    date: {type: Date, default: Date.now}
+                    waitlistCount: { type: Number, required: true },
+                    date: { type: Date, default: Date.now }
                 }
             ],
             openSeatHistory: [
                 {
-                    openCount: {type: Number, required: true},
-                    date: {type: Date, default: Date.now}
+                    openCount: { type: Number, required: true },
+                    date: { type: Date, default: Date.now }
                 }
             ],
             holdFileHistory: [
                 {
-                    holdFileCount: {type: Number, required: true},
-                    date: {type: Date, default: Date.now}
+                    holdFileCount: { type: Number, required: true },
+                    date: { type: Date, default: Date.now }
                 }
             ],
             users: [
-                {email: {type: String, required: true}},
+                { email: { type: String, required: true } },
             ], default: [],
         }
     ],
-    
-}, {timestamps: true});
+
+}, { timestamps: true });
 
 const Course = mongoose.models.Course || mongoose.model('Course', courseSchema);
 

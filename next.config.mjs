401447671731a1a2e217a1b763f3634502d5b684
vaidya45 <103296422+vaidya45@users.noreply.github.com>
2024-02-18 10:13:25 -0500

    Removed puppeteer and chromium

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        serverComponentsExternalPackages: ['puppeteer-core', '@sparticuz/chromium']
-    }
+
 };
 
 export default nextConfig;
